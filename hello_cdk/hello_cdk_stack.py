982dc510ad93021ddd531b94fc44f8d8b3a583b6
Clarence Ho <clarence@skywidesoft.com>
2019-06-02 16:01:29 +0800

    Initial commit

--- hello_cdk/hello_cdk_stack.py
+++ hello_cdk/hello_cdk_stack.py
@@ -1,9 +1,16 @@
-from aws_cdk import cdk
-
+from aws_cdk import (
+    cdk,
+    aws_s3 as s3
+)
 
 class HelloCdkStack(cdk.Stack):
 
     def __init__(self, app: cdk.App, id: str, **kwargs) -> None:
         super().__init__(app, id)
 
-        # The code that defines your stack goes here
+        # S3 bucket for this stack
+        bucket = s3.Bucket(self, 
+            "MyFirstBucket", 
+            bucket_name="my-cdk-bucket",
+            versioned=True,
+            encryption=s3.BucketEncryption.KmsManaged,)
